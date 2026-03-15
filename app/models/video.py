01f2f35f483ead81523321ecd41825b4da6c2e17
Salil Mandal <salilmandal908@gmail>
2026-03-15 23:36:38 +0530

    Models

--- app/models/video.py
+++ app/models/video.py
@@ -1,6 +1,5 @@
 from pydantic import BaseModel
-from typing import List
-
+from typing import List, Optional
 
 class VideoItem(BaseModel):
     title: str
@@ -8,6 +7,38 @@ class VideoItem(BaseModel):
     channel: str
     published_at: str
     url: str
+
+
+class Thumbnail(BaseModel):
+    url: str
+    width: int
+    height: int
+
+class ContentDetails(BaseModel):
+        totalItemCount:int
+        newItemCount: int
+        activityType: str
+
+class SubscriberSnippet(BaseModel):
+         title: str
+         description: str
+         channelId: str
+         thumbnails: Thumbnail
+     
+class PageInfo(BaseModel):
+    totalResults: int
+    resultsPerPage: int
+
+class SubscriptionResource(BaseModel):
+    id: Optional[str]
+    kind: Optional[str] 
+
+class ResourceId(BaseModel):
+     kind: str
+     channel_id = str
+
+class Subscription_list(BaseModel):
+
 
 
 class LikedVideosResponse(BaseModel):
